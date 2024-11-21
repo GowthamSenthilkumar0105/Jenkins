27bfe554051aebf7595fcdf76eccf3114adf35d6
GowthamSenthilkumar0105 <gowthamsenthilkumar0105@gmail.com>
2024-11-21 11:01:50 +0530

    first-commit

---
+++ src/main/java/com/Jenkins/JenkinsController.java
@@ -0,0 +1,13 @@
+package com.Jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+	@GetMapping(value="/get")
+	public String get() {
+		return "Hello World";
+	}
+
+}
